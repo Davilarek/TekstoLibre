728a20cb8db886bcb87d11f263e4788b778f9e22
Daniel <davilareko@gmail.com>
2024-05-27 02:50:52 +0200

    Some stuff

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const port = process.env.PORT || 7778;
 const http = require('http');
 const fs = require("fs");
 const path = require('path');
+const HTMLParser = require('node-html-parser');
+const myRequire = require;
+const allowJS = true;
+
+function evalInScope(js, contextAsScope) {
+    // @ts-ignore
+    // eslint-disable-next-line quotes
+    return new Function(["contextAsScope", "js"], "return (function() { with(this) { return eval(js); } }).call(contextAsScope)")(contextAsScope, js);
+}
 
 /**
  * @param {http.IncomingMessage} req
@@ -30,11 +39,37 @@ const requestHandler = (req, res) => {
         res.end();
         return;
     }
+    if (!allowJS) {
+        if (test.endsWith(".js")) {
+            res.writeHead(404);
+            res.end();
+            return;
+        }
+    }
     if (fs.existsSync(test) && !test.endsWith(path.sep)) {
         fs.createReadStream(test).pipe(res);
         return;
     }
     // res.end(fs.readFileSync("./index.html"));
+    if (!allowJS) {
+        const rawHTML = fs.readFileSync("./index.html", "utf8");
+        const root = HTMLParser.parse(rawHTML);
+        const globals = {
+            get require() {
+                return myRequire;
+            },
+            globalThis: {
+                get document() {
+                    return root;
+                },
+                localStorage: undefined,
+            },
+        };
+        evalInScope(fs.readFileSync("./main.js", "utf8"), { ...globals, ...globals.globalThis });
+        res.write(root.toString());
+        res.end();
+        return;
+    }
     fs.createReadStream("./index.html").pipe(res);
 };
 
